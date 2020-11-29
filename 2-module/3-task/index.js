bc829f61883408bcfd74cbd791218c23e777a9c9
nikita <e6wuk1990@mail.ru>
2020-11-29 12:04:54 +0200

    решение 3-ей задачи второго модуля

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,14 @@
 let calculator = {
-  // ваш код
+  read(one,two) {
+    this.one = one;
+    this.two = two;
+  },
+  sum() {
+    return this.one + this.two; 
+  },
+  mul() {
+    return this.one * this.two;
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
