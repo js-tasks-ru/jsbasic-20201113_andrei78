17f44813e9da69df019bd5fee35175875be2e598
nikita <e6wuk1990@mail.ru>
2020-12-07 00:08:00 +0200

    решение задач 4-го модуля

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,5 +3,11 @@
  * @returns {string}
  */
 function camelize(str) {
-  // ваш код...
+  return str.split('-').map((item,index) => {
+    if (index == 0) {
+      return item;
+    }
+    return item[0].toUpperCase() + item.slice(1)}
+  ).join('');
+ 
 }
--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,7 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+  for (let i = 0; i < table.rows.length; i++) {
+    td = table.rows[i].cells[i].style.backgroundColor = 'red';
+  }
 }
