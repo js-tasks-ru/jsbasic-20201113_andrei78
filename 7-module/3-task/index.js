50590b095299baea87e515c022957e90e26ac633
nikita <e6wuk1990@mail.ru>
2021-01-13 14:47:12 +0200

    исправил замечания препожавателя ( изменил путь до файла на стандартный)

--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,7 +1,7 @@
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
     this.steps = steps;
-    this.value = value;
+    this.value = Math.min(value, (steps - 1));
     this.elem = this.render();
    
   }
